43f554fcd71e7c36df472d49d8323a9573cdcbd0
Murray Cumming <murrayc@murrayc.com>
2017-06-05 22:57:29 +0200

    Reformat with gofmt -w

--- src/math/sieve_of_eratosthenes/sieve_of_eratosthenes.go
+++ src/math/sieve_of_eratosthenes/sieve_of_eratosthenes.go
@@ -1,43 +1,41 @@
 package main
 
 import (
-  "fmt"
-  "math"
+	"fmt"
+	"math"
 )
 
-
 func print_primes(n int) {
-  var not_prime = make([]bool, n);
+	var not_prime = make([]bool, n)
 
-  sq := int(math.Sqrt(float64(n)))
+	sq := int(math.Sqrt(float64(n)))
 
-  // Mark all multiples as not primes.
-  for i := 2; i <= sq; i++ {
-    // If i is already marked as not prime then all its multiples have been
-    // marked already too.
-    if not_prime[i] {
-      continue;
-    }
+	// Mark all multiples as not primes.
+	for i := 2; i <= sq; i++ {
+		// If i is already marked as not prime then all its multiples have been
+		// marked already too.
+		if not_prime[i] {
+			continue
+		}
 
-    // Start at i squared.
-    j := i * i;
+		// Start at i squared.
+		j := i * i
 
-    for j < n {
-      not_prime[j] = true
-      j += i;
-    }
-  }
+		for j < n {
+			not_prime[j] = true
+			j += i
+		}
+	}
 
-  // Print all the numbers that we have not ruled out.
-  for i := 2; i < n; i++ {
-    if !not_prime[i] {
-      fmt.Printf("%v ", i)
-    }
-  }
-  fmt.Print("\n");
+	// Print all the numbers that we have not ruled out.
+	for i := 2; i < n; i++ {
+		if !not_prime[i] {
+			fmt.Printf("%v ", i)
+		}
+	}
+	fmt.Print("\n")
 }
 
-
 func main() {
-  print_primes(1000)
+	print_primes(1000)
 }
