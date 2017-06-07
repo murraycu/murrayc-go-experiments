0b9bdf5d041ce443e5f2c429f75e8e1cf629845c
Murray Cumming <murrayc@murrayc.com>
2017-06-07 11:32:10 +0200

    Return multiple values.

--- src/arrays/binary_search/binary_search.go
+++ src/arrays/binary_search/binary_search.go
@@ -1,11 +1,11 @@
 package main
 
-func binary_search_lo_hi(a []int, lo int, hi int, value int) int {
+func binary_search_lo_hi(a []int, lo int, hi int, value int) (int, bool) {
 	for lo <= hi {
 		mid := lo + ((hi - lo) / 2)
 		midval := a[mid]
 		if midval == value {
-			return mid
+			return mid, true
 		} else if value < midval {
 			hi = mid
 		} else {
@@ -13,13 +13,13 @@ func binary_search_lo_hi(a []int, lo int, hi int, value int) int {
 		}
 	}
 
-	return -1
+	return -1, false
 }
 
-func binary_search(a []int, value int) int {
+func binary_search(a []int, value int) (int, bool) {
 	hi := len(a)
 	if hi < 0 {
-		return -1
+		return -1, false
 	}
 
 	return binary_search_lo_hi(a, 0, hi-1, value)
--- src/arrays/binary_search/binary_search_test.go
+++ src/arrays/binary_search/binary_search_test.go
@@ -6,7 +6,13 @@ import (
 
 func TestBinarySearch(t *testing.T) {
 	var a = []int{0, 2, 4, 7, 9, 11, 15}
-	if binary_search(a, 11) != 5 {
-		t.Error("binary_search() returned unexpected result.")
+	pos, found := binary_search(a, 11)
+
+	if !found {
+		t.Error("binary_search() didn't find the value.")
+	}
+
+	if pos != 5 {
+		t.Error("binary_search() returned unexpected position.")
 	}
 }
